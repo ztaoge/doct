c28175065cb19c1e477cb6d32dd04857f02a3303
zhangjintao <zhangjintao@octinn.com>
2016-10-09 17:07:39 +0800

    add repository

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 
 $em = require_once "bootstrap.php";
 
+/** @var \models\Repository\ProductRepository $productRepository */
 $productRepository = $em->getRepository('models\Entity\Product');
-var_dump($productRepository->test());
+var_dump($productRepository->getAllProduct());
+
+//$product = new models\Entity\Product();
+//$product->setName('产品1号');
+//$em->persist($product);
+//$em->flush();
--- models/Repository/ProductRepository.php
+++ models/Repository/ProductRepository.php
@@ -6,13 +6,18 @@
  * Time: 15:14
  */
 namespace models\Repository;
-
 use Doctrine\ORM\EntityRepository;
 
 class ProductRepository extends EntityRepository
 {
+    public function getAllProduct()
+    {
+        $dql = 'SELECT p FROM models\Entity\Product p';
+        return $this->_em->createQuery($dql)->getArrayResult();
+    }
+
     public function test()
     {
-        echo 1;
+        return 'test';
     }
 }
